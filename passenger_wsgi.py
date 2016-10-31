087e721f88897554a44c462f9230c10915e14bf0
antwoo13 <antwoo13@administrator.dreamhost.com>
2016-10-31 16:43:23 -0700

    empty flask site

---
+++ passenger_wsgi.py
@@ -0,0 +1,15 @@
+import sys, os
+INTERP = os.path.join(os.environ['HOME'], 'purchaseorders.fleurametztoronto.com', 'bin', 'python')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+sys.path.append(os.getcwd())
+
+
+from flask import Flask
+application = Flask(__name__)
+
+
+
+@application.route('/')
+def index():
+    return 'Hello Passenger'

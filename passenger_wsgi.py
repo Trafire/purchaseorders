fd62c80504771e9aeaa70f00f19724cdbff5ea49
antwoo13 <antwoo13@administrator.dreamhost.com>
2016-10-31 17:18:51 -0700

    Implemented sample with changes https://mattcarrier.com/flask-dreamhost-setup/

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,18 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'myappname.domain.com', 'bin', 'python')
+INTERP = os.path.join(os.environ['HOME'], 'purchaseorders.fleurametztoronto.com', 'bin', 'python')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
+
+
+from flask import Flask
+application = Flask(__name__)
+
+
+
+@application.route('/')
+def index():
+    return 'Hello Passenger'
 
 
 sys.path.append('myappname')

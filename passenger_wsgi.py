f7a5d4898f441d14eb974e328fee391ad127ab7b
antwoo13 <antwoo13@administrator.dreamhost.com>
2016-11-05 09:22:03 -0700

    both login and / got to login page

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,7 +8,7 @@ sys.path.append(os.getcwd())
 from flask import Flask, render_template
 application = Flask(__name__)
 
-
+@application.route('/login')
 @application.route('/')
 def index():
     return render_template("login.html")

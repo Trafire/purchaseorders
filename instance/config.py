62c747ede2f6e19f1d0004f64bf65128da072300
Antoine Wood <awood@fleurametz.com>
2016-11-11 17:04:16 -0500

    added to config

--- instance/config.py
+++ instance/config.py
@@ -2,3 +2,6 @@ DATABASE_USER = 'fm_antoine'
 DATABASE_PASSWORD = '%antoine'
 DATABASE_LOCATION = 'mysql.torontofleurametz.com'
 DATABASE_NAME =  'fm_purchaseorders '
+
+SQLALCHEMY_DATABASE_URI = DATABASE_LOCATION
+CSRF_ENABLED = True

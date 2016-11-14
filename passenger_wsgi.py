1cf63e36518081a31f1fae794025d771ca9b8489
Antoine Wood <awood@fleurametz.com>
2016-11-14 11:31:27 -0500

    data base connect

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -16,6 +16,28 @@ application = Flask(__name__)
 application.config.from_object('config')
 application.config.from_pyfile('config.py')
 
+############## database setup ###################
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION   
+db = SQLAlchemy(application)    
+
+
+
+############## Routes ###################
+
 @application.route('/login',methods=["POST","GET"])
 def login():
     if request.method == 'POST': 

5add61a23f8a04e79160b6cd98ff790f4bd4745c
antwoo13 <awood@fleurametz.com>
2016-11-16 13:49:28 -0800

    basic login system complete!!!!!!!!

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from passenger_wsgi import db, bcrypt
 from flask_user import UserMixin
 from flask_user.forms import RegisterForm
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import Form, BooleanField, StringField, PasswordField, validators,TextField
 from sqlalchemy.ext.hybrid import hybrid_property
 
 class User(db.Model, UserMixin):
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
 
     # User Authentication information
     username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False, default='')
+    _password = db.Column(db.String(64), nullable=False)
     reset_password_token = db.Column(db.String(100), nullable=False, default='')
 
     # User Email information
@@ -27,6 +27,20 @@ class User(db.Model, UserMixin):
 
     def is_active(self):
       return self.is_enabled
+
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def _set_password(self, plaintext):
+        self._password = bcrypt.generate_password_hash(plaintext)
+
+    def is_correct_password(self, plaintext):
+        if bcrypt.check_password_hash(self._password, plaintext):
+            return True
+
+        return False
 
 
 
@@ -50,3 +64,9 @@ class MyRegisterForm(RegisterForm):
     ])
     email = StringField('Email', validators=[validators.Email('An FM Email is required')]) 
     confirm = PasswordField('Confirm Password')	
+
+
+class UsernamePasswordForm(Form):
+    username = StringField('Username', validators=[validators.Required("Must Provide Username")])
+    password = PasswordField('Password', validators=[validators.Required("Must Enter a password")])
+    
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -11,6 +11,8 @@ from flask import Flask, render_template, request,redirect, url_for, flash
 from flask_mail import Mail
 from flask_user import login_required , UserManager, UserMixin, SQLAlchemyAdapter
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user
+
 
 application = Flask(__name__)
 application.config.from_pyfile('config.py')
@@ -32,13 +34,40 @@ from model import *
 db_adapter = SQLAlchemyAdapter(db, User)        # Register the User model
 user_manager = UserManager(db_adapter, application,register_form=MyRegisterForm)     # Initialize Flask-User
 
+
+########### Login management############
+#from model import User
+login_manager = LoginManager()
+login_manager.init_app(application)
+login_manager.login_view =  "login"
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.filter(User.id == userid).first()
+
 ############## Routes ###################
 @application.route('/login',methods=["POST","GET"])
 def login():
-    if request.method == 'POST': 
-        return render_template("index.html")
-    else:
-        return render_template("login.html")
+    form = UsernamePasswordForm(request.form)
+    if request.method == "POST":
+        user = User.query.filter_by(username=form.username.data).first_or_404()
+	if user.is_correct_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('members'))
+        else:
+
+            return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ))
+
 
 @application.route('/')
 def index():
@@ -48,9 +77,11 @@ def index():
 @application.route('/register', methods=['GET', 'POST'])
 def register():
     form = MyRegisterForm(request.form)
-    if request.method == 'POST':# and form.validate():
+    form.validate()
+    flash_errors(form)
+    if form.validate_on_submit(): #request.method == 'POST' and form.validate():
         user = User(first_name=form.first_name.data, last_name=form.last_name.data, username=form.username.data, email=form.email.data,password=form.password.data)
-#	return "after user"
+ #	return "after user"
         db.session.add(user)
 	db.session.commit()
 
@@ -59,9 +90,10 @@ def register():
     return render_template('register.html', form=form)
             
 @application.route('/members')
-#@login_required
-def members_page():
-    return render_template_string("members.html")
+@login_required
+def members():
+    
+    return render_template("members.html")
 
 
             
